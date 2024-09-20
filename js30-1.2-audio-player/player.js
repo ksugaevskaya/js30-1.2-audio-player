6124f0d772adaf7ac0f8e795fb825cb536b7f087
ksugaevskaya <ksu.gaevskaya@gmail.com>
2024-09-20 17:15:38 -0500

    feat: add play/pause functional

--- js30-1.2-audio-player/player.js
+++ js30-1.2-audio-player/player.js
@@ -2,17 +2,21 @@ const playButton = document.getElementById("play-button");
 const stopButton = document.getElementById("stop-button");
 const forwardButton = document.getElementById("forward-button");
 const backwardButton = document.getElementById("backward-button");
+const song = document.getElementById("song");
 
 const playButtonClicked = () => {
   console.log("Play Button clicked!");
   playButton.className = "hidden";
   stopButton.className = "music-buttons";
+  song.currentTime = 0;
+  song.play();
 };
 
 const stopButtonClicked = () => {
   console.log("Stop Button clicked!");
   stopButton.className = "hidden";
   playButton.className = "music-buttons";
+  song.pause();
 };
 
 const forwardButtonClicked = () => {
