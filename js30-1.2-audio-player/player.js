b384ad0d61488338d13fdf1fcb45722d7770e0d7
ksugaevskaya <ksu.gaevskaya@gmail.com>
2024-09-21 06:42:18 -0500

    feat: add background button switch

--- js30-1.2-audio-player/player.js
+++ js30-1.2-audio-player/player.js
@@ -3,8 +3,21 @@ const stopButton = document.getElementById("stop-button");
 const forwardButton = document.getElementById("forward-button");
 const backwardButton = document.getElementById("backward-button");
 const song = document.getElementById("song");
+const artwork = document.getElementById("artwork");
+const artist = document.getElementById("artist");
+const songTitle = document.getElementById("song-title");
+const blurredBackgroundImage = document.getElementById(
+  "blurred-background-image"
+);
 
 let playNum = 0;
+
+const updateSongInfo = () => {
+  artwork.src = tracks[playNum].artwork;
+  artist.innerHTML = tracks[playNum].artist;
+  songTitle.innerHTML = tracks[playNum].songTitle;
+  blurredBackgroundImage.style.backgroundImage = `url(${tracks[playNum].artwork})`;
+};
 
 const playButtonClicked = () => {
   console.log("Play Button clicked!");
@@ -27,6 +40,7 @@ const forwardButtonClicked = () => {
   if (playNum > tracks.length - 1) {
     playNum = 0;
   }
+  updateSongInfo();
   console.log("Forward Button clicked!");
 };
 
@@ -35,6 +49,7 @@ const backwardButtonClicked = () => {
   if (playNum < 0) {
     playNum = tracks.length - 1;
   }
+  updateSongInfo();
   console.log("Backward Button clicked!");
 };
 
